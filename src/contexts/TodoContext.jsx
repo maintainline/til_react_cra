61a0bb7adde775c8731d0842869868e6cf553323
maintainline <z.seon.dev@gmail.com>
2025-07-28 12:33:46 +0900

    [docs] Context API와 usReducer의 이해

---
+++ src/contexts/TodoContext.jsx
@@ -0,0 +1,8 @@
+// 1.  todo 를 위한 context 생성
+
+const { createContext } = require("react");
+
+// 1.1. Todo 데이터를 위한 context
+export const TodoStateContext = createContext(null);
+// 1.2. Todo 데이터 업데이트를 위한 context
+export const TodoDispatchContext = createContext(null);
